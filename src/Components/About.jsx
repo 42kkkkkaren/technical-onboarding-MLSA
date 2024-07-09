2d218985fdf3e4837f42c833b94279fa9a5d6d30
Karen Ketlyn Ferreira Barcelos <72056327+42kkkkkaren@users.noreply.github.com>
2024-07-09 16:19:11 +0000

    Commit the technical onboarding site

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,24 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/185.-microsoft.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "microsoft logo with blank background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm Computer Scientist at Federal University from São Carlos (UFSCar) and i've made a 2 years internship at Microsoft.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "Cloud Computing",
+  "DevOps",
+  "Kotlin",
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/professionalfoto.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "photo of me with red coat";
 
 /**
  * Project list
